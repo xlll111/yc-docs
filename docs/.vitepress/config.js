a118e1829b0d90cfc0319981259ce30d1308dcbb
xl <qwaeszrdxtfc123@163.com>
2026-02-27 23:42:37 +0800

    0

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,45 @@
+export default {
+  base: "/yc-docs/",
+  // 网站标题
+  title: "YC文档",
+  // 网站描述
+  description: "一个简洁美观的文档网站",
+
+  // 主题配置
+  themeConfig: {
+    // 网站左上角导航
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "指南", link: "/guide/getting-started" },
+      { text: "GitHub", link: "https://github.com" },
+    ],
+
+    // 侧边栏
+    sidebar: {
+      "/guide/": [
+        {
+          text: "指南",
+          items: [
+            { text: "快速开始", link: "/guide/getting-started" },
+            { text: "配置", link: "/guide/configuration" },
+            { text: "控制台", link: "/guide/console" },
+          ],
+        },
+        {
+          text: "详细指南",
+          // 可以折叠的组
+          collapsed: false, // true表示默认折叠
+          items: [
+            {
+              text: "设置",
+              link: "/guide/detailed/setting",
+            },
+          ],
+        },
+      ],
+    },
+
+    // 社交链接
+    socialLinks: [{ icon: "github", link: "https://github.com" }],
+  },
+};
---
+++ docs/guide/getting-started.md
@@ -0,0 +1,16 @@
+# 快速开始
+
+欢迎使用YC文档！本文将帮助你快速上手。
+
+## 安装
+
+通过下载器安装
+
+https://class.xlll.dpdns.org/api/files/installer/download/latest
+
+## 使用
+
+安装完成后，程序将：
+
+- 自动添加到开机自启动项
+- 自动启动并完成初始化
